12bbc7f6f7fe1467a87bf6abc64db3757cf59d10
Ward Van Driessche <ward.vandriessche@centrica.com>
2024-01-15 18:01:17 +0100

    Add docs after comment from Monty about missing code. Don't fully agree

--- ops.py
+++ ops.py
@@ -2,12 +2,41 @@
 
 
 def add_numbers(x: float, y: float) -> float:
+    """
+    Adds two numbers.
+
+    Parameters:
+        x (float): The first number.
+        y (float): The second number.
+
+    Returns:
+        float: The sum of x and y.
+    """
     return x + y
 
 
-def square_number(x: float) -> float:
-    return x**2
-
-
 def substract_numbers(x: float, y: float) -> float:
+    """
+    Subtracts one number from another.
+
+    Parameters:
+        x (float): The number to be subtracted from.
+        y (float): The number to subtract.
+
+    Returns:
+        float: The result of subtracting y from x.
+    """
     return x - y
+
+
+def square_number(x: float) -> float:
+    """
+    Squares numbers
+
+    Parameters:
+        x (float): The number to be squared.
+
+    Returns:
+        float: The result of the square.
+    """
+    return x**2
